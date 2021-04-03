7ed93c4fe941b5e619f7e48eccc550968af96853
ramakrishna0505 <ramakrishna1870@gmail.com>
2021-04-04 02:38:45 +0530

    Day 4 Apr 4 2021

---
+++ stringShuffle.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let result= new Array(s.length).fill(0);
+    for(let i=0;i<s.length;i++){
+        result[indices[i]] = s[i];
+    }
+    return result.join('');
+};
