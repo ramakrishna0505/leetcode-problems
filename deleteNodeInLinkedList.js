48687f2b9b1fc26d1af74f8115a19dd076442326
ramakrishna0505 <ramakrishna1870@gmail.com>
2021-04-14 08:35:34 +0530

    Day 8 Apr 14 2021

---
+++ deleteNodeInLinkedList.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function(node) {
+    while(node.next!==null){
+        if(node.next.next!==null){
+            node.val = node.next.val;
+            node = node.next;
+        } else {
+            node.val = node.next.val;
+            node.next = null;
+        }
+    }
+};
